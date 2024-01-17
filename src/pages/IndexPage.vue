bdde2401b5dfc6572a743fadb90130b7c2395dd4
JacksonBowe <jacksonbowe98@gmail.com>
2024-01-17 18:57:29 +1000

    init

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- Change below between `row` and `column`. I would expect them to behave identically -->
+  	<q-page padding class="bg-orange row">
+		<div class="col bg-pink column">
+			<div class="bg-green col-1">Why?</div>
+		</div>
+  	</q-page>
+</template>
+
+<script setup lang="ts">
+</script>
